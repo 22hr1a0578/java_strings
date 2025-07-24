5fe000bd0502984584baebedc7918356fad7a029
N.latha <154883112+22hr1a0578@users.noreply.github.com>
2025-07-24 13:50:07 +0530

    Create newLine.java

---
+++ newLine.java
@@ -0,0 +1,7 @@
+class Main{
+    public static void main(String args[]){
+        String str="HI HELLO";
+        String newLine=System.lineSeparator();
+        System.out.print(newLine+str);
+    }
+}
